aeb52deed372dc50e5b58e3b84ebbf4d62076cca
Robani <muhammadrobbani77@gmail.com>
2024-06-01 12:47:03 +0700

    partial navbar template

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:type" content="article">
+    <meta property="og:site_name" content="V-Lab Manajemen Menengah">
+    <meta property="og:title" content="Virtual Lab Manajemen Menengah - Universitas Gunadarma">
+    <meta name="keywords" content="vlab, vlab mamen, V-Lab, Mamen, mamen, vlab gundar, vlab gunadarma">
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}">
+    <title>V-Lab Manajemen Menengah - {{ $pageTitle }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    @include('layout.partials.navbar.blade')
+    @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#06b6d4', //Cyan-500
+                secondary: '#64748b', //Slate-500
+                dark: '#1f2937' //Gray-800
+            },
+        },
+    },
+    plugins: [],
+}
